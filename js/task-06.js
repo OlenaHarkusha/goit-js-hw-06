378cd4c386abfea98e03cb2a4d696feafb8627b7
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-02-11 12:00:09 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,5 +3,6 @@ const inputEl = document.querySelector("#validation-input");
 inputEl.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
+  inputEl.classList.remove("valid", "invalid");
   event.currentTarget.value.length === Number(event.currentTarget.dataset.length) ? inputEl.classList.add("valid") : inputEl.classList.add("invalid");
 }
